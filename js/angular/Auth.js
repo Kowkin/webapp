826f9f7c08fe51ef19c449a9d5dfdb0ec3198d7f
Kowkin <kowkin89@gmail.com>
2015-11-18 19:27:58 +0200

    update

---
+++ js/angular/Auth.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kowki on 18.11.2015.
+ */
---
+++ js/angular/controllers.js
@@ -0,0 +1,67 @@
+/**
+ * Created by kowki on 18.11.2015.
+ */
+'use strict';
+
+/* Controllers */
+var webApp = angular.module('webApp', ['ngRoute']);
+
+webApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: '/view/singin.html',
+            controller: 'SingInCtrl',
+        })
+        .when('#/edit-profile', {
+            templateUrl: 'view/edit-profile.html',
+            controller: 'ProfileCtrl',
+        })
+        .when('#/edit_menu', {
+            templateUrl: 'view/edit_menu.html',
+            controller: 'MenuListCtrl',
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}]);
+
+//CONTROLLERS
+
+webApp.controller('DishListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+    $http.get('dish/dish.json').success(function(data, status, headers, config) {
+        $scope.dish = data;
+    });
+
+}]);
+
+
+//SingInCtrl
+webApp.controller('SingInCtrl',['$scope', '$http', '$location', function($scope, $rootScope, $http, $location, AUTH_EVENTS, AuthService) {
+    $scope.credentials = {
+        username: '',
+        password: ''
+    };
+    $scope.login = function (credentials) {
+        AuthService.login(credentials).then(function () {
+            $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+        }, function () {
+            $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+        });
+    };
+}]);
+
+
+//ProfileCtrl
+webApp.controller('ProfileCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+
+
+//menuListCtrl
+webApp.controller('MenuListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+
+
+
