5a71688ce4797a9d94029389c07f00c3d1145b0d
Kowkin <kowkin89@gmail.com>
2015-11-22 01:58:09 +0200

    first commit

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,36 +1,41 @@
-/**
- * Created by kowki on 18.11.2015.
- */
 'use strict';
 
 /* Controllers */
-var webApp = angular.module('webApp', ['ngRoute']);
+var webApp = angular.module('webApp', ['ngRoute', 'ngAnimate', 'toaster']);
 
 webApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
-        .when('/', {
-            templateUrl: '/view/singin.html',
-            controller: 'SingInCtrl'
-        })
         .when('/login', {
             title: 'Login',
-            templateUrl: 'partials/login.html',
+            templateUrl: 'view/login.html',
             controller: 'authCtrl'
         })
-        .when('/singup', {
-            templateUrl: '/view/singup.html',
-            controller: 'SingUpCtrl'
+        .when('/logout', {
+            title: 'Logout',
+            templateUrl: 'view/login.html',
+            controller: 'logoutCtrl'
+        })
+        .when('/signup', {
+            title: 'Signup',
+            templateUrl: 'view/signup.html',
+            controller: 'authCtrl'
         })
         .when('/edit-profile', {
             templateUrl: 'view/edit-profile.html',
-            controller: 'ProfileCtrl'
+            controller: 'authCtrl'
         })
         .when('/edit-menu', {
             templateUrl: 'view/edit-menu.html',
             controller: 'MenuListCtrl'
         })
+        .when('/', {
+            title: 'Login',
+            templateUrl: 'view/login.html',
+            controller: 'authCtrl',
+            role: '0'
+        })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/login'
         })
 }]);
 
@@ -41,8 +46,41 @@ webApp.controller('SingInCtrl',['$scope', '$http', '$location', function($scope,
 
 }]);
 
-//ProfileCtrl
-webApp.controller('ProfileCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+//authCtrl
+webApp.controller('authCtrl', function ($scope, $rootScope, $routeParams, $location, $http, Data) {
+    //initially set those objects to null to avoid undefined error
+    $scope.login = {};
+    $scope.signup = {};
+    $scope.doLogin = function (customer) {
+        Data.post('login', {
+            customer: customer
+        }).then(function (results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+    $scope.signup = {name:'',password:'',weight:'',date:'',gender:'',illness:'',limit:''};
+    $scope.signUp = function (customer) {
+        Data.post('signUp', {
+            customer: customer
+        }).then(function (results) {
+            Data.toast(results);
+            if (results.status == "success") {
+                $location.path('dashboard');
+            }
+        });
+    };
+    $scope.logout = function () {
+        Data.get('logout').then(function (results) {
+            Data.toast(results);
+            $location.path('login');
+        });
+    }
+});
+
+webApp.controller('authCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
     $http.get('data/illness.json').success(function(data) {
         $scope.illness = data;
     });
